a406811fa2ed0e9be6280b918648378ed517b746
jafar70 <jafsalami@hotmail.co.uk>
2020-11-29 15:27:47 +0000

    Created the single portfolio page

---
+++ wp-content/themes/jafar/modules/m07-single-portfolio.php
@@ -0,0 +1,107 @@
+<?php
+
+$portfolio_image = get_field('portfolio_image');
+$overview = get_field('portfolio_item_overview');
+$responsibilities = get_field('portfolio_item_responsibilities');
+$outcome = get_field('portfolio_item_outcome');
+$project_link = get_field('project_link');
+$portfolio_github_link = get_field('portfolio_github_link');
+$disclaimer = get_field('project_disclaimer');
+
+?>
+
+<div class="m07">
+  <div class="container">
+    <div class="m07__title">
+      <h1 class='banner-title m07__title__text'><?php echo esc_html( the_title() . '.' ); ?></h1>
+    </div>
+
+    <div class='m07__meta'>
+      <div class="m07__meta__item">
+        <p><a href="/portfolio" class='arrow-back'><?php echo esc_html( 'Portfolio' ); ?></a><?php echo esc_html( ' / ' . get_the_title() ); ?></p>
+      </div>
+
+      <div class="m07__meta__item">
+          <p class='categories'>
+            <?php
+              $categories = get_the_category();
+                foreach( $categories as $category) {
+                  $name = $category->name;
+                  echo esc_html( " $name " );
+                }
+              ?>  
+          </p>
+      </div>
+    </div>
+
+    <?php if( !empty($portfolio_image) ): ?>
+      <img data-src="<?php echo $portfolio_image['url']; ?>" alt="<?php echo $portfolio_image['alt']; ?>" class="m07__img mb-40 lazy"/>
+    <?php endif; ?>
+
+    <div class='m07__grid'>
+      <div class="m07__grid__text">
+        <?php if ( $overview ) : ?>
+          <h3><?php echo esc_html( 'Overview:' ); ?></h3>
+          <?php echo wp_kses_post( $overview ); ?>
+        <?php endif; ?>
+
+        <?php if ( $responsibilities ) : ?>
+          <h3><?php echo esc_html( 'My Responsibilities:' ); ?></h3>
+          <?php echo wp_kses_post( $responsibilities ); ?>
+        <?php endif; ?>
+
+        <?php if ( $outcome ) : ?>
+          <h3><?php echo esc_html( 'The Outcome:' ); ?></h3>
+          <?php echo wp_kses_post( $outcome ); ?>
+        <?php endif; ?>
+      </div>
+
+      <div class="m07__grid__skills">    
+        <?php if( have_rows('technologies_used') ): ?>
+          <h3><?php echo esc_html( 'Technologies I Used:' ); ?></h3>
+          <ul class="technologies-list">
+          <?php while( have_rows('technologies_used') ): the_row(); 
+            $technology_name = get_sub_field('technology_name');
+            ?>
+            <li> <?php echo esc_html( $technology_name ); ?></li>
+          <?php endwhile; ?>
+          </ul>
+        <?php endif; ?>
+      </div>
+
+      <div class="m07__grid__buttons">
+            <?php if( $project_link ): ?>
+              <a class="button" href='<?php echo esc_html( $project_link ); ?>' rel="noopener" target='_blank'><?php echo esc_html( 'View Website' ); ?></a>
+            <?php endif; ?>
+        
+            <?php if( $portfolio_github_link ): ?>
+              <a class="button" href='<?php echo $portfolio_github_link; ?>' rel="noopener" target='_blank'><?php echo esc_html( 'View Source Code' ); ?></a>
+            <?php else : ?>
+              <a class="button button--disabled" rel="noopener" target='_blank' type="button" disabled><?php echo esc_html( 'No Source Code' ); ?></a>
+            <?php endif; ?>
+
+        <?php if( $disclaimer ): ?>
+          <p class='m07__grid__buttons__disclaimer'><?php echo esc_html( $disclaimer ); ?></p>
+        <?php endif; ?>
+      </div>
+                
+      <div class="m07__grid__nav">
+        <?php
+          $prev = get_adjacent_post(false, '', true);
+          $next = get_adjacent_post(false, '', false);
+      
+          if($prev){
+            $url = get_permalink($prev->ID);            
+            echo '<div class="m07__grid__nav__item m07__grid__nav__item--left"><a href="' . $url . '" title="' . $prev->post_title . '" class="previous-post-link"><span>&#171;</span><p> Previous project</p></a></div>';
+          }
+      
+          if($next) {
+            $url = get_permalink($next->ID);            
+            echo '<div class="m07__grid__nav__item m07__grid__nav__item--right"><a href="' . $url . '" title="' . $next->post_title . '" class="next-post-link"><p>Next project </p> <span>&#187;<span></a></div>';
+          } 
+        ?>
+      </div>
+      </div>
+    </div>
+  </div> 
+</div>
---
+++ wp-content/themes/jafar/single-portfolio.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: Single Portfolio Template
+*/
+
+
+get_header(); 
+
+get_template_part( 'modules/m07', 'single-portfolio' );
+
+get_footer();
+?>
