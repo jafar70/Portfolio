e6da1aee0d16f818302d909be0f2fa668edcc5f3
jafar70 <jafsalami@hotmail.co.uk>
2020-11-29 15:27:30 +0000

    Created the single portfolio page

--- wp-content/themes/jafar/global-modules/gm01-desktop-menu.php
+++ wp-content/themes/jafar/global-modules/gm01-desktop-menu.php
@@ -1,7 +1,7 @@
 <header class="gm01">
     <div class="container gm01__grid">
     <div class="mobile-whitespace"></div>
-    <a class="gm01__grid__logo logo" href="<?php echo get_site_url(); ?>"> <h2>Jafar.</h2></a>
+    <a class="gm01__grid__logo logo" href="<?php echo esc_url( get_site_url() ); ?>"> <h2>Jafar.</h2></a>
     
     <nav class="gm01__grid__menu">
         <?php 
@@ -23,17 +23,17 @@
             </div>  
             <?php if(get_field('instagram_url', 'option')) { ?>
                 <a href="<?php echo get_field('instagram_url', 'option'); ?>" rel="noopener" target="_blank" class='nav-right__link' aria-label="Instagram">
-                    <?php echo file_get_contents( get_template_directory() . '/assets/img/instagram.svg' ); ?>
+                    <?php get_template_part('assets/img/inline', 'instagram.svg'); ?>
                 </a>
             <?php } ?>
             <?php if(get_field('github_url', 'option')) { ?>
                 <a href="<?php echo get_field('github_url', 'option'); ?>" rel="noopener" target="_blank" class='nav-right__link' aria-label="Github">
-                    <?php echo file_get_contents( get_template_directory() . '/assets/img/github.svg' ); ?>
+                <?php get_template_part('assets/img/inline', 'github.svg'); ?>
                 </a>
             <?php } ?>
             <?php if(get_field('linkedin_url', 'option')) { ?>
                 <a href="<?php echo get_field('linkedin_url', 'option'); ?>" rel="noopener" target="_blank" class='nav-right__link' aria-label="LinkedIn">
-                <?php echo file_get_contents( get_template_directory() . '/assets/img/linkedin.svg' ); ?>
+                <?php get_template_part('assets/img/inline', 'linkedin.svg'); ?>
                 </a>
             <?php } ?>
         
--- wp-content/themes/jafar/global-modules/gm04-footer.php
+++ wp-content/themes/jafar/global-modules/gm04-footer.php
@@ -16,25 +16,29 @@ $twitter_url = get_field('twitter_url', 'option');
       <div class="gm04__grid__social-media">
         <?php if($instagram_url) { ?>
           <a class="gm04__links" href="<?php echo esc_url($instagram_url); ?>" rel="noopener" target="_blank" aria-label="Instagram">
-            <?php echo file_get_contents( get_template_directory() . '/assets/img/instagram.svg' ); ?><?php echo esc_html('Instagram'); ?>
+            <?php get_template_part('assets/img/inline', 'instagram.svg'); ?>
+            <?php echo esc_html('Instagram'); ?>
           </a>
         <?php } ?>
 
         <?php if($github_url) { ?>
           <a class="gm04__links" href="<?php echo esc_url($instagram_url); ?>" rel="noopener"  target="_blank" aria-label="Github">
-            <?php echo file_get_contents( get_template_directory() . '/assets/img/github.svg' ); ?><?php echo esc_html('Github'); ?>
+          <?php get_template_part('assets/img/inline', 'github.svg'); ?>
+          <?php echo esc_html('Github'); ?>
           </a>
         <?php } ?>
 
         <?php if($linkedin_url) { ?>
           <a class="gm04__links" href="<?php echo esc_url($linkedin_url); ?>" rel="noopener" target="_blank" aria-label="LinkedIn">
-            <?php echo file_get_contents( get_template_directory() . '/assets/img/linkedin.svg' ); ?><?php echo esc_html('LinkedIn'); ?>
+          <?php get_template_part('assets/img/inline', 'linkedin.svg'); ?>
+            <?php echo esc_html('LinkedIn'); ?>
           </a>  
         <?php } ?>
 
         <?php if($twitter_url) { ?>
           <a class="gm04__links" href="<?php echo esc_url($twitter_url); ?>" rel="noopener" target="_blank" aria-label="Twitter">
-          <?php echo file_get_contents( get_template_directory() . '/assets/img/twitter.svg' ); ?><?php echo esc_html('Twitter'); ?>
+          <?php get_template_part('assets/img/inline', 'twitter.svg'); ?>
+          <?php echo esc_html('Twitter'); ?>
           </a>
         <?php } ?>  
       </div>
@@ -42,7 +46,7 @@ $twitter_url = get_field('twitter_url', 'option');
         <a href='mailto:jafsalami@hotmail.co.uk' class="gm04__links" target='_blank'>
           <?php echo esc_html('jafsalami@hotmail.co.uk'); ?>
         </a>
-        <p class='mb-0'><b><?php echo esc_html('© ' . date("Y") . ' Jafar Salami'); ?></b></p>
+        <p class='mb-0'><?php echo esc_html('© ' . date("Y") . ' Jafar Salami'); ?></p>
       </div>
     </div>
   </div>
