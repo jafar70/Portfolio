18251de809a51a29ba961483194efd6542acb33f
Jafar Salami <jafar@alchemy.digital>
2023-09-22 12:28:14 +0100

    Setting up new environment.

--- wp-content/plugins/optimole-wp/assets/build/dashboard/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '367a62fbbf766c7a7b04');
